0066b540eae89fc8b2eecbfdbc097e978f679da5
Jocelin HOUNON <hounonj@gmail.com>
2021-06-10 13:21:10 +0100

    init res

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <span class="text-brand-black">Connard !</span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+});
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#647a6e",
+          black: "#2c3e50",
+        },
+      },
+      fontFamily: {
+        futura: "Futura, sans-serif",
+        poppins: "Poppins, sans-serif",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
